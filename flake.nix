806bd55b24479d9e39e0da13d665eba530fc7bdd
John Doe <jeremi@daym0ns.com>
2025-11-30 18:15:30 +0100

    added depend

--- flake.nix
+++ flake.nix
@@ -68,6 +68,7 @@ EOF
         buildInputs = [
           prev.xorg.libX11
           prev.xorg.libXcursor
+          prev.libXft.dev
           prev.fontconfig.dev
           prev.xorg.libXinerama
         ];
